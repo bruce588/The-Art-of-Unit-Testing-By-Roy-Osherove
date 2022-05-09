b8d18c967a9f67b4a24efd0352aff5b36b82a32a
bruce588 <bruce588@gmail.com>
2022-05-10 05:18:03 +0800

    add lab

--- ArtOfUnitTesting2ndEd.Samples/NSubExamples/Listing5-3.cs
+++ ArtOfUnitTesting2ndEd.Samples/NSubExamples/Listing5-3.cs
@@ -1,4 +1,5 @@
-﻿using Chapter5.LogAn;
+﻿using System;
+using Chapter5.LogAn;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -17,7 +18,24 @@ namespace NSubExamples
             analyzer.MinNameLength = 6;
             analyzer.Analyze("a.txt");
 
-           logger.Received().LogError("Filename too short: a.txt");
+           logger.Received().LogError("Filename too short: a.txt");//不傳入參數,比對完全一致
+           
+        }
+        
+        [Test]
+        public void Analyze_BruceLab_CallLogger()
+        {
+            ILogger logger = Substitute.For<ILogger>();
+
+            LogAnalyzer analyzer = new LogAnalyzer(logger);
+
+            analyzer.MinNameLength = 6;
+            string testFileName = "a.txt";
+            analyzer.Analyze(testFileName);
+
+            //測試Received()方法在呼叫LogError時,傳入的參數
+            logger.Received().LogError(Arg.Is<string>(x=>x.Contains(testFileName)));
+           
         }
 
         [Test]
